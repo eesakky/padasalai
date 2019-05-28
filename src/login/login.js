e5c298f0530d99ac0fc387146ec5e19af696a430
Esakkimuthu E <esakki.er@gmail.com>
2019-05-29 00:46:00 +0530

    initial commit for login page

---
+++ src/login/login.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import TextInput from "../controls/input";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <TextInput />
+            </div>
+        );
+    }
+}
+
+export default Login;
